bc8ba0003cbe95b49e09dd7105914c6ed85fdd74
Christopher Peplin <peplin@bueda.com>
2010-10-01 23:15:41 -0400

    Line wrap.

--- neb/relationship.py
+++ neb/relationship.py
@@ -1,9 +1,10 @@
 from neb.api import TrinityResource
 
 class Relationship(TrinityResource):
-    def create(self, start, to, type, **kwargs):
-        params = dict(start=start, to=to, type=type, data=kwargs)
-        return self.post(self._relationship_path(start), payload=params)
+    def create(self, from_node, to, link_type, **kwargs):
+        params = dict(from_node=from_node, to=to, link_type=link_type,
+                data=kwargs)
+        return self.post(self._relationship_path(from_node), payload=params)
 
     @staticmethod
     def _relationship_path(node_id, relationship_id=None):
