36a4af3781d4256304cc37224342609b1f9312c9
Christopher Peplin <peplin@bueda.com>
2010-10-12 23:25:07 -0400

    Add test case for appended relationship.

--- neb/tests/test_relationship.py
+++ neb/tests/test_relationship.py
@@ -7,20 +7,21 @@ from neb.relationship import Relationship
 from neb.api import TrinityResource
 import neb
 
-fake_relationship = {'from_node': 'peplin', 'to': 'bueda',
-        'data': {'other': 'data'}, 'link_type': 'works_for'}
-
 class RelationshipTests(unittest.TestCase):
+    fake_relationship = {'from_node': 'peplin', 'to': 'bueda',
+            'data': {'other': 'data'}, 'link_type': 'works_for'}
+
     def setUp(self):
         super(RelationshipTests, self).setUp()
         mockito.when(TrinityResource).request(
                 mockito.any(), path=mockito.any(), payload=mockito.any(),
-                headers=mockito.any()).thenReturn(json.dumps(fake_relationship))
+                headers=mockito.any()).thenReturn(
+                    json.dumps(self.fake_relationship))
         self.relationship = Relationship().create(
-                from_node=fake_relationship['from_node'],
-                to=fake_relationship['to'],
-                link_type=fake_relationship['link_type'],
-                **fake_relationship['data'])
+                from_node=self.fake_relationship['from_node'],
+                to=self.fake_relationship['to'],
+                link_type=self.fake_relationship['link_type'],
+                **self.fake_relationship['data'])
 
     def tearDown(self):
         super(RelationshipTests, self).tearDown()
@@ -30,13 +31,41 @@ class RelationshipTests(unittest.TestCase):
         assert self.relationship
 
     def test_from(self):
-        eq_(self.relationship.from_node, fake_relationship['from_node'])
+        eq_(self.relationship.from_node, self.fake_relationship['from_node'])
 
     def test_to(self):
-        eq_(self.relationship.to, fake_relationship['to'])
+        eq_(self.relationship.to, self.fake_relationship['to'])
 
     def test_data(self):
-        eq_(self.relationship.data, fake_relationship['data'])
+        eq_(self.relationship.data, self.fake_relationship['data'])
 
     def test_type(self):
-        eq_(self.relationship.link_type, fake_relationship['link_type'])
+        eq_(self.relationship.link_type, self.fake_relationship['link_type'])
+
+class ExtendedRelationshipTests(unittest.TestCase):
+    fake_relationship = {'from_node': 'peplin', 'to': 'bueda',
+            'data': {'other': 'data', 'new_data': 'banf'},
+            'link_type': 'works_for'}
+
+    def setUp(self):
+        super(ExtendedRelationshipTests, self).setUp()
+        mockito.when(TrinityResource).request(
+                mockito.any(), path=mockito.any(), payload=mockito.any(),
+                headers=mockito.any()).thenReturn(
+                    json.dumps(self.fake_relationship))
+        self.relationship = Relationship().create(
+                from_node=self.fake_relationship['from_node'],
+                to=self.fake_relationship['to'],
+                link_type=self.fake_relationship['link_type'],
+                append=True,
+                **self.fake_relationship['data'])
+
+    def tearDown(self):
+        super(ExtendedRelationshipTests, self).tearDown()
+        mockito.unstub()
+
+    def test_create(self):
+        assert self.relationship
+
+    def test_data(self):
+        eq_(self.relationship.data, self.fake_relationship['data'])
